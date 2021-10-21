277b8988f70016d1af63b490ba2f2070e150615c
Thomas Thomassen <thomas@thomthom.net>
2021-10-21 15:52:14 +0200

    Add fmt vcpkg for comparison.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
+#include <fmt/core.h>
 #include <opensubdiv/version.h>
 
 int main()
 {
-  std::cout << "Hello OpenSubDiv\n";
-  std::cout << "  Version: " <<
-    OPENSUBDIV_VERSION_MAJOR << "." <<
-    OPENSUBDIV_VERSION_MINOR << "." <<
-    OPENSUBDIV_VERSION_PATCH << "\n";
+  fmt::print("Hello OpenSubDiv\n");
+  fmt::print("  Version: {}.{}.{}\n",
+      OPENSUBDIV_VERSION_MAJOR,
+      OPENSUBDIV_VERSION_MINOR,
+      OPENSUBDIV_VERSION_PATCH);
   return 0;
 }
